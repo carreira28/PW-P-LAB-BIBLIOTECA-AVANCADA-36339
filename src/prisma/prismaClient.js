2198e422f3746761c7b23470e324552b6bc8a6f9
carreira28 <carreira907@gmail.com>
2026-03-17 15:51:17 +0000

    .

--- src/prisma/prismaClient.js
+++ src/prisma/prismaClient.js
@@ -1,6 +1,10 @@
+require("dotenv").config();
+
 const { PrismaClient } = require("@prisma/client");
 const { withAccelerate } = require("@prisma/extension-accelerate");
 
-const prisma = new PrismaClient().$extends(withAccelerate());
+const prisma = new PrismaClient({
+  accelerateUrl: process.env.DATABASE_URL,
+}).$extends(withAccelerate());
 
 module.exports = prisma;
